fa308fe0e9496b06f353eb3343cd00e64c8d8f4f
jgbs <joaogabrieldebrittoesilva@gmail.com>
2015-06-11 22:32:11 -0300

    Criando a DAO de unidade e metodos de listar as unidades

---
+++ saudf/saudfhub/saudfhub.Shared/UnidadeDAO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace saudfhub
+{
+    class UnidadeDAO
+    {
+        public List<Unidade> Listar()
+        {    
+            return Conexao.Conn().Table<Unidade>().ToList();
+        }
+
+        public static List<Unidade> Listar(string nome)
+        {
+            return Conexao.Conn().Table<Unidade>().Where(x => x.Nome.Contains(nome)).ToList();
+            //return Conexao.Conn().Query<Unidade>("SELECT * FROM Unidade WHERE Nome = ?", nome).ToList();
+        }
+
+        public List<Unidade> Listar(string porNome, string eBairro)
+        {
+            return Conexao.Conn().Table<Unidade>().Where(x => x.Nome.Contains(porNome) && x.Bairro.Contains(eBairro)).ToList();
+        }
+
+        //public List<Unidade> Listar(Expression<Func<Unidade, bool>> filtro)
+        //{
+        //    //para filtros mais especificos
+        //    return Conexao.Conn().Table<Unidade>().Where(filtro).ToList();
+        //}
+
+        //public Unidade Buscar(int id)
+        //{
+        //    return Listar(x => x.IdUnidade == id).FirstOrDefault();
+        //}
+    }
+}
